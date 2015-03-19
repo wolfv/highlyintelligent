f338b774108df9eda2527a0300ba4e3a0a65eb03
Wolf Vollprecht <w.vollprecht@gmail.com>
2015-03-19 23:58:02 +0100

    working on

--- project_2/run.py
+++ project_2/run.py
@@ -11,7 +11,8 @@ from sklearn import datasets
 from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
 from sklearn.svm import LinearSVC
 from sklearn.cross_validation import train_test_split
-
+from sklearn.preprocessing import LabelBinarizer, MultiLabelBinarizer
+from sklearn.ensemble import RandomForestClassifier
 ipython = False
 try:
     import IPython as ip
@@ -21,7 +22,8 @@ except:
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--testfile", "-t", help="Select XML file")
-parser.add_argument("--samplesize", "-s", help="Select XML file", default=1)
+parser.add_argument("--samplesize", "-s", help="Choose how many runs", default=1)
+parser.add_argument("--interactive", "-i", help="Interactive?", default=False)
 
 
 def read_file(filename):
@@ -61,7 +63,10 @@ def binarize(val, steps, append_to=None):
 
 def run(args):
     X_read = read_file("data/train.csv")
+
     Y_read = read_file("data/train_y.csv")
+
+    lb = LabelBinarizer()
 
     X_data = X_read[:,9:]
     Y_data = Y_read
@@ -69,24 +74,38 @@ def run(args):
     X_data = binarize(X_read[:, 1], 5, append_to=X_data)
     X_data = binarize(X_read[:, 2], 5, append_to=X_data)
     X_data = binarize(X_read[:, 3], 5, append_to=X_data)
-    X_data = binarize(X_read[:, 4], 5, append_to=X_data)
-    X_data = binarize(X_read[:, 5], 5, append_to=X_data)
+    X_data = binarize(X_read[:, 4], 10, append_to=X_data)
+    # X_data = binarize(X_read[:, 5], 10, append_to=X_data)
     X_data = binarize(X_read[:, 6], 5, append_to=X_data)
-    X_data = binarize(X_read[:, 7], 5, append_to=X_data)
-    X_data = binarize(X_read[:, 8], 5, append_to=X_data)
-    X_data = binarize(X_read[:, 9], 5, append_to=X_data)
-    
+    X_data = binarize(X_read[:, 7], 10, append_to=X_data)
+    X_data = binarize(X_read[:, 8], 20, append_to=X_data)
+    # X_data = binarize(X_read[:, 9], 5, append_to=X_data)
+
     for i in range(0, int(args.samplesize)):
         X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size=0.2)
-
-        classifier_y0 = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X_train, Y_train[:,0])
-        prediction = np.zeros(Y_test.shape)
-        prediction[:,0] = classifier_y0.predict(X_test)
         
-        classifier_y1 = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X_train, Y_train[:,1])
-        prediction[:,1] = classifier_y1.predict(X_test)
+        labels = np.left_shift(Y_train[:,0], 4) + Y_train[:,1]
 
-        print("%s Run, Error: %s" % (i, error(Y_test, prediction)))
+        # lb.set_params("classes_", ["a", "b"])
+        # bin_labels = lb.transform(Y_train)
+        classifier_y1 = RandomForestClassifier(n_estimators=40).fit(X_train, labels)
+        # classifier_y0 = OneVsRestClassifier(LinearSVC(random_state=0), n_jobs=-1).fit(X_train, labels)
+        prediction = classifier_y1.predict(X_test)
+        result = np.zeros((prediction.shape[0], 2))
+        result[:, 0] = np.transpose(np.right_shift(prediction, 4))
+        result[:, 1] = np.transpose(np.bitwise_and(prediction, 15))
+        # print(col_a, col_b )
+        # prediction = np.hstack((col_a, col_b))
+        print("PREDICTION")
+        print(result)
+        # prediction = np.zeros(Y_test.shape)
+        # prediction[:,0] = classifier_y0.predict(X_test)
+        
+        # X_train = binarize(classifier_y0.predict(X_train), 7, append_to=X_train)
+        # classifier_y1 = OneVsRestClassifier(LinearSVC(random_state=0)).fit(X_train, Y_train[:,1])
+        # prediction[:,1] = classifier_y1.predict(binarize(prediction[:,0], 7, X_test))
+
+        print("%s Run, Error: %s" % (i, error(Y_test, result)))
 
     if args.testfile:
         X_testfile = read_file(args.testfile)
@@ -97,7 +116,7 @@ def run(args):
         np.savetxt('result.txt', Y_testfile_prediction,  fmt='%i', delimiter=",")
 
     try:
-        if ipython:
+        if ipython and args.interactive:
             ip.embed()
     except:
         print("Well, you're supposed to have iPython but something went wrong...")
