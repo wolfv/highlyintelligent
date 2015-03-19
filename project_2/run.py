86ecc17c544a97083dd8f2ab0a35d8b1812fc624
Wolf Vollprecht <w.vollprecht@gmail.com>
2015-03-19 17:48:18 +0100

    implemented file reader

--- project_2/run.py
+++ project_2/run.py
@@ -2,5 +2,38 @@
 
 import numpy as np
 
-from matplotlib import plot as plt
+from matplotlib import pyplot as plt
+
+import argparse
+import csv
+
+ipython = False
+try:
+	import IPython as ip
+	ipython = True
+except:
+	print("No IPython, wie schade ... :(")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--testfile", "-t", help="Select XML file")
+
+
+def read_file(filename):
+	result = []
+	with open(filename) as train_fp:
+		reader = csv.reader(train_fp)
+		for row in reader:
+			result.append([int(el) for el in row])
+
+	return np.array(result)
+
+def run():
+	X_read = read_file("data/train.csv")
+
+	if ipython:
+		ip.embed()
+
+if __name__ == '__main__':
+	run()
+
 
